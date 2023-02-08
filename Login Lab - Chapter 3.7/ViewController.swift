ff7d929ec2313c5d121ba60eab57cfc9f0708747
Paige Stephenson <paigestephenson29@gmail.com>
2023-02-07 22:01:21 -0700

    add final storyboard and code for login lab

--- Login Lab - Chapter 3.7/ViewController.swift
+++ Login Lab - Chapter 3.7/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameEntry: UITextField!
+    
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var forgotUserNameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title =
+            "Forgot Password"
+        } else if sender == forgotUserNameButton {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = userNameEntry.text
+        }
+    }
 
-
+    @IBAction func forgotPassword(_ sender: Any) {
+        performSegue(withIdentifier: "loginToLandingView", sender: sender)
+    }
+    
+    @IBAction func forgotUserName(_ sender: Any) {
+        performSegue(withIdentifier: "loginToLandingView", sender: sender)
+    }
+    
+    
 }
 
